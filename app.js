ba5ff7038d86f89157b3c7e36555cc0f4f2817e5
cimpal55 <cimpals71@gmail.com>
2021-03-09 16:05:26 +0200

    12 lesson

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const list = document.querySelector('#book-list ul');
+const forms = document.forms;
 
 list.addEventListener('click', (e) => {
   if(e.target.className == 'delete'){
@@ -7,17 +8,19 @@ list.addEventListener('click', (e) => {
   }
 });
 
-const forms = document.forms;
-console.log(forms);
-console.log(forms['add-book']);
-
-Array.from(forms).forEach(function(form){
-  console.log(form);
-});
-
 const addForm = forms['add-book'];
 addForm.addEventListener('submit', function(e){
   e.preventDefault();
+
   const value = addForm.querySelector('input[type="text"]').value;
-  console.log(value);
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
+
+  bookName.textContent = value;
+  deleteBtn.textContent = 'delete';
+
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
 });
