2c059a884d94908265593a8095f3802bb22e9af1
cimpal55 <cimpals71@gmail.com>
2021-03-16 14:18:53 +0200

    17 lesson

--- app.js
+++ app.js
@@ -57,3 +57,19 @@ searchBar.addEventListener('keyup', function(e){
     }
   })
 })
+
+// tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', function(e){
+  if(e.target.tagName == "Ll"){
+    const targetPanel = document.querySelector(e.target.dataset.target);
+    panels.forEach(function(panel){
+      if (panel == targetPanel){
+        panel.classList.add('active');
+      } else {
+        panel.classList.remove('active');
+      }
+    })
+  }
+})
