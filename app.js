eada3285b0d5d8e95e5f7d13414a22774bae0027
cimpal55 <cimpals71@gmail.com>
2021-03-16 14:05:35 +0200

    15 lesson

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ addForm.addEventListener('submit', function(e){
   const bookName = document.createElement('span');
   const deleteBtn = document.createElement('span');
 
-// add content
+// add text content
   bookName.textContent = value;
   deleteBtn.textContent = 'delete';
 
@@ -31,4 +31,14 @@ addForm.addEventListener('submit', function(e){
   li.appendChild(bookName);
   li.appendChild(deleteBtn);
   list.appendChild(li);
+});
+
+// hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+  if(hideBox.checked){
+    list.style.display = "none";
+  } else {
+    list.style.display = "initial";
+  }
 });
